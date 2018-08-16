80d85f2f8addd1d2ea4455b5fa71b7f1adea5048
aikin <1@aikin.me>
2018-08-17 00:10:44 +0800

    🌟 feat(GENETATOR): should can generate cli project

--- src/actions/generator.ts
+++ src/actions/generator.ts
@@ -1,5 +1,6 @@
 import chalk from 'chalk'
 import * as fs from 'fs-extra'
+import * as mustache from 'mustache'
 import * as ora from 'ora'
 import * as path from 'path'
 
@@ -11,6 +12,22 @@ export const generate = credentials => {
 			const spinner = ora('Copying cli seed...').start()
 
 			await fs.copy(`${cwd}/seed`, '.')
+
+			const packageContent = fs.readFileSync(`./package.json`, 'utf8')
+			const updatedPackageContent = mustache.render(packageContent, credentials)
+			fs.writeFileSync('./package.json', updatedPackageContent)
+
+			const cliTestContent = fs.readFileSync(`./src/__test__/cli.ts`, 'utf8')
+			const updatedCliTestContent = mustache.render(cliTestContent, credentials)
+			fs.writeFileSync('./src/__test__/cli.ts', updatedCliTestContent)
+
+			const cliContent = fs.readFileSync(`./bin/cli_`, 'utf8')
+			const updatedCliContent = mustache.render(cliContent, credentials)
+			fs.writeFileSync('./bin/cli_', updatedCliContent)
+
+			fs.renameSync('./src/__test__/cli.ts', `./src/__test__/${credentials.cliName}.ts`)
+			fs.renameSync('./src/cli.ts', `./src/${credentials.cliName}.ts`)
+			fs.renameSync('./bin/cli_', `./bin/${credentials.cliName}`)
 
 			spinner.succeed()
 		} catch (err) {
--- src/seed/src/__test__/cli.ts
+++ src/seed/src/__test__/cli.ts
@@ -4,7 +4,7 @@ import * as path from 'path'
 
 const cwd = path.dirname(__dirname)
 const packageJson = require(path.join(cwd, '../package.json'))
-const cli = (args, opts) => execa(path.join(cwd, '../bin/cli_'), args, opts)
+const cli = (args, opts) => execa(path.join(cwd, '../bin/{{cliName}}'), args, opts)
 
 test('version', async t => {
 	const { stdout } = await cli(['-V'], { cwd })
@@ -13,5 +13,5 @@ test('version', async t => {
 
 test('help', async t => {
 	const { stdout } = await cli(['-h'], { cwd })
-	t.regex(stdout, /Usage: cli_ /)
+	t.regex(stdout, /Usage: {{cliName}}/)
 })
