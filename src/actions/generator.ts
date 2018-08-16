9fd7d51e60c6a785d2f611a3ea65e247b86d0b52
aikin <1@aikin.me>
2018-08-16 20:45:38 +0800

    🌟 feat(SEED): should copy seed to dist

--- src/actions/generator.ts
+++ src/actions/generator.ts
@@ -1,3 +1,22 @@
+import chalk from 'chalk'
+import * as fs from 'fs-extra'
+import * as ora from 'ora'
+import * as path from 'path'
+
+const cwd = path.dirname(__dirname)
+
 export const generate = credentials => {
-	console.log(credentials)
+	;(async () => {
+		console.log('credentials', credentials)
+		console.log('credentials', cwd)
+		try {
+			const spinner = ora('Copying files...').start()
+
+			await fs.copy(`${cwd}/seed`, '.')
+
+			spinner.stop()
+		} catch (err) {
+			console.log(chalk.redBright(err))
+		}
+	})()
 }
